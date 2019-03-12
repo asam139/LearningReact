0090a95eb4d50316121334bc1b793e99d06b05b8
Saul Moreno Abril <saul@appfluence.com>
2019-03-12 15:13:14 +0100

    Fetching Deal details

--- ReactNative/Bakesale/src/ajax.js
+++ ReactNative/Bakesale/src/ajax.js
@@ -3,9 +3,16 @@ const apiHost = 'https://bakesaleforgood.com';
 export default {
   async fetchInitialDeals() {
     try {
-      let response = await fetch(
-        apiHost + '/api/deals',
-      );
+      const response = await fetch( apiHost + '/api/deals');
+      return await response.json();
+    } catch (error) {
+      console.error(error);
+    }
+  },
+
+  async fetchDealDetail(dealId) {
+    try {
+      const response = await fetch( apiHost + '/api/deals/' + dealId);
       return await response.json();
     } catch (error) {
       console.error(error);
--- ReactNative/Bakesale/src/components/DealDetail.js
+++ ReactNative/Bakesale/src/components/DealDetail.js
@@ -2,8 +2,9 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {Text, View, Image, TouchableOpacity, StyleSheet} from 'react-native';
 import {priceDisplay} from "../utils";
+import ajax from '../ajax';
 
-class DealDetail extends Component {
+export default class DealDetail extends Component {
   static propTypes = {
     initialDealData: PropTypes.object.isRequired,
   };
@@ -12,16 +13,36 @@ class DealDetail extends Component {
     deal: this.props.initialDealData,
   };
 
+  async componentDidMount() {
+    const fullDeal = await ajax.fetchDealDetail(this.state.deal.key);
+    this.setState({
+      deal: fullDeal
+    })
+  }
+
   render() {
     const { deal } = this.state;
     return (
       <View style={styles.deal}>
         <Image source={{ uri: deal.media[0] }} style={styles.image} />
-        <View style={styles.info}>
-          <Text style={styles.title}>{deal.title}</Text>
+        <View style={styles.detail}>
+          <View>
+            <Text style={styles.title}>{deal.title}</Text>
+          </View>
           <View style={styles.footer}>
-            <Text style={styles.cause}>{deal.cause.name}</Text>
-            <Text style={styles.price}>{priceDisplay(deal.price)}</Text>
+            <View style={styles.info}>
+              <Text style={styles.price}>{priceDisplay(deal.price)}</Text>
+              <Text style={styles.cause}>{deal.cause.name}</Text>
+            </View>
+            {deal.user && (
+              <View style={styles.user}>
+                <Image source={{ uri: deal.user.avatar }} style={styles.avatar} />
+                <Text>{deal.user.name}</Text>
+              </View>
+            )}
+          </View>
+          <View style={styles.description}>
+            <Text>{deal.description}</Text>
           </View>
         </View>
       </View>
@@ -29,38 +50,56 @@ class DealDetail extends Component {
   }
 }
 
+
 const styles = StyleSheet.create({
   deal: {
+    backgroundColor: 'white',
     marginHorizontal: 12,
     marginTop: 50,
+    borderColor: '#bbbb',
+    borderWidth: 1,
   },
   image: {
     width: '100%',
     height: 150,
-    backgroundColor: '#ccc',
+    backgroundColor: '#cccc',
   },
-  info: {
-    padding: 10,
-    backgroundColor: '#fff',
-    borderColor: '#bbb',
-    borderWidth: 1,
-    borderTopWidth: 0,
+  detail: {
   },
   title: {
     fontSize: 16,
+    padding: 10,
     fontWeight: 'bold',
-    marginBottom: 5,
+    backgroundColor: 'rgba(237, 149, 45, 0.4)',
   },
   footer: {
     flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    marginTop: 15,
+  },
+  info: {
+    alignItems: 'center',
+  },
+  user: {
+    alignItems: 'center',
   },
   cause: {
-    flex: 2,
+    marginVertical: 10,
   },
   price: {
-    flex: 1,
-    textAlign: 'right',
+    fontWeight: 'bold'
+  },
+  avatar: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+  },
+  description: {
+    borderColor: '#dddd',
+    borderWidth: 1,
+    borderStyle: 'dotted',
+    margin: 10,
+    padding: 10,
   },
 });
-
-export default DealDetail;
