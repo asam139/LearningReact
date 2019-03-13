728c2e52b2eecc919d1cd56e7d2c3e0657e8f5c2
Saul Moreno Abril <saul@appfluence.com>
2019-03-13 12:53:19 +0100

    Finishing swipe animation

--- ReactNative/Bakesale/src/components/DealDetail.js
+++ ReactNative/Bakesale/src/components/DealDetail.js
@@ -2,13 +2,15 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {
   Text,
+  Button,
   View,
   Image,
   TouchableOpacity,
   PanResponder,
   Animated,
   Dimensions,
-  StyleSheet
+  StyleSheet,
+  Linking
 } from 'react-native';
 import {priceDisplay} from "../utils";
 import ajax from '../ajax';
@@ -16,6 +18,7 @@ import ajax from '../ajax';
 const dimensions = Dimensions.get('window');
 
 export default class DealDetail extends Component {
+  imageXPos = new Animated.Value(0);
   imagePanResponder = PanResponder.create({
     // Ask to be the responder:
     onStartShouldSetPanResponder: () => true,
@@ -24,14 +27,46 @@ export default class DealDetail extends Component {
       // The most recent move distance is gestureState.move{X,Y}
       // The accumulated gesture distance since becoming responder is
       // gestureState.d{x,y}
-      console.log('Moving', gestureState.dx);
+      this.imageXPos.setValue(gestureState.dx);
     },
     onPanResponderRelease: (evt, gestureState) => {
       // The user has released all touches while this view is the
       // responder. This typically means a gesture has succeeded
-      console.log('Released');
+      const width = dimensions.width;
+      if (Math.abs(gestureState.dx) > width * 0.4) {
+        const direction = Math.sign(gestureState.dx);
+        // -1 for left, 1 for right
+        Animated.timing(this.imageXPos, {
+          toValue: direction * width,
+          duration: 250
+        }).start(() => this.handleSwipe(-1 * direction));
+      } else {
+        Animated.spring(this.imageXPos, {
+          toValue: 0,
+        }).start();
+      }
     },
   });
+
+  handleSwipe = (indexDirection) => {
+    if (!this.state.deal.media[this.state.imageIndex + indexDirection]) {
+      Animated.spring(this.imageXPos, {
+        toValue: 0,
+      }).start();
+      return;
+    }
+    this.setState((prevState) => ({
+      imageIndex: prevState.imageIndex + indexDirection
+    }), () => {
+      // Next image animation
+      const width = dimensions.width;
+      this.imageXPos.setValue(indexDirection * width);
+      Animated.spring(this.imageXPos, {
+        toValue: 0,
+      }).start();
+    });
+  };
+
   static propTypes = {
     initialDealData: PropTypes.object.isRequired,
     onBack: PropTypes.func.isRequired,
@@ -49,6 +84,10 @@ export default class DealDetail extends Component {
     })
   }
 
+  openDealUrl = () => {
+    Linking.openURL(this.state.deal.url)
+  };
+
   render() {
     const { deal } = this.state;
     return (
@@ -56,9 +95,9 @@ export default class DealDetail extends Component {
         <TouchableOpacity onPress={this.props.onBack}>
           <Text style={styles.backLink}>Back</Text>
         </TouchableOpacity>
-        <Image
+        <Animated.Image
           {...this.imagePanResponder.panHandlers}
-          style={styles.image}
+          style={[{ left: this.imageXPos }, styles.image] }
           source={{ uri: deal.media[this.state.imageIndex] }}
         />
         <View style={styles.detail}>
@@ -80,6 +119,7 @@ export default class DealDetail extends Component {
           <View style={styles.description}>
             <Text>{deal.description}</Text>
           </View>
+          <Button title='Buy this deal!' onPress={this.openDealUrl}/>
         </View>
       </View>
     );
