05130c8d2a246226462c44e17a6faaecf680b3da
Saul Moreno Abril <saul@appfluence.com>
2019-03-13 16:38:22 +0100

    My first reducer

--- Redux/Ch01/01_03/start/src/index.js
+++ Redux/Ch01/01_03/start/src/index.js
@@ -1,14 +1,17 @@
 import C from './constants'
-import { allSkiDays, goal} from './initialState.json'
+import { goal } from './store/reducers'
+
+const state = 10
+
+const action = {
+  type: C.SET_GOAL,
+  payload: 15
+}
+
+const nextState = goal(state, action)
 
 console.log(`
-
-  Ski Day Counter
-  ================
-  The goal is ${goal} days
-  Initially there are ${allSkiDays.length} ski days in state
-  
-  Constants
-  ================
-  Count: ${Object.keys(C).join('\n    ')}
+  Initial goal: ${state}
+  action: ${JSON.stringify(action)}
+  new goal: ${nextState}
 `)
