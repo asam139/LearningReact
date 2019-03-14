8913387add4e3975808d288d48818561f45bc03a
Saul Moreno Abril <saul@appfluence.com>
2019-03-14 13:20:36 +0100

    Unsubscribe from the store

--- Redux/ski-day-counter/src/index.js
+++ Redux/ski-day-counter/src/index.js
@@ -32,3 +32,24 @@ store.dispatch({
   type: C.SET_GOAL,
   payload: 20
 });*/
+
+const unsubcribeGoalLogger = store.subscribe(() => {
+  console.log(`
+    Goal: ${store.getState().goal}
+  `)
+});
+
+setInterval(() => {
+
+  store.dispatch({
+    type: C.SET_GOAL,
+    payload: Math.floor(Math.random() * 100)
+  });
+
+}, 250);
+
+setInterval(() => {
+
+  unsubcribeGoalLogger();
+
+}, 3000);
