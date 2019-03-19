0f093269fb68cefb8640bff0f14ff25c646fe2e4
Saul Moreno Abril <saul@appfluence.com>
2019-03-19 11:07:40 +0100

    Connect React with Redux

--- react-redux-tutorial/src/index.js
+++ react-redux-tutorial/src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
 import App from './components/App';
 import store from './store';
-import { getReadableStories } from './selectors/story';
-import { doArchiveStory } from './actions/archive';
+import './index.css';
 
 function render() {
   ReactDOM.render(
-    <App
-      stories={getReadableStories(store.getState())}
-      onArchive={ (id) => { store.dispatch(doArchiveStory(id)) } }
-    />,
+    <Provider store={store}>
+      <App/>
+    </Provider>,
     document.getElementById('root')
   );
 }
